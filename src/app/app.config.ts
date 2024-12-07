6c1157ad012317c015db034a4c11015ef3765d11
sbrugel <sbrugel62@gmail.com>
2024-12-07 15:36:27 -0500

    Add firebase functionality to app config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,6 +4,17 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { provideStorage, getStorage } from '@angular/fire/storage';
+import { firebaseConfig } from '../firebase-config'; // Adjust based on your file structure
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes), provideAnimationsAsync(), 
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage())
+  ]
 };
